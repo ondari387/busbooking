070cf21671cd20347e36c7ce9bf45cf0e27d81e4
ondari367 <ondari.mike@gmail.com>
2020-03-16 07:09:10 +0300

    vhanged Heading and footer caption

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
   <link rel="stylesheet" type="text/css" href="css/cover.css">
   <div class="cover-container d-flex w-100 h-100 p-1 m-auto flex-column">
       <main role="main" class="inner-cover">
-        <h3 class="m-5" style="color: #04304B;">Travel with us this Holiday</h3>
+        <h3 class="m-5" style="color: #04304B;">Travel with us in style</h3>
         <img src="images/bus1.png" class="container-fluid col-md-4" height="250px"><br>
           <a href="booking.php" class="btn btn-lg btn-dark m-2">Book our classy buses</a>
       </main>
 
       <footer class="mastfoot mt-auto">
-          <p class="mute" style="color: #04304B;"><i>Travel in style</i></p>
+          <p class="mute" style="color: #04304B;"><i>Travel Cool</i></p>
       </footer>
   </div>
 
